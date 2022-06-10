6a7bdfc438851b152685de4e786df7d02799cde1
Oleksandr Nykytin <keeper@ninydev.com>
2022-06-10 19:12:28 +0300

    Create

--- 22_backStudent/controllers/studentsController.js
+++ 22_backStudent/controllers/studentsController.js
@@ -1,11 +1,36 @@
+// Получить библиотечную функцию для генерации ID
+const lib = require ("../lib/guid")
+const generateUUID = lib.generateUUID
+
+
 // Список доступных студентов (массив объектов)
 let students = [
-    {id: 1, name: 'Sasha', age:40},
-    {id: 2, name: 'Vasya', age:50},
+    {_id: generateUUID(), name: 'Sasha', age:40},
+    {_id: generateUUID(), name: 'Vasya', age:50},
 ];
 
-// вернуть всех студентов
-// Read (All)
+/**
+ * Создать нового студента
+ * Create === POST
+ * @param request
+ * @param response
+ */
+exports.create = function (request, response) {
+    // console.log(request.body)
+    // Получили нового студента на сервере
+    let newStudent = request.body
+    newStudent._id = generateUUID()
+    students.push(newStudent)
+    return response.status(201).json(newStudent);
+}
+
+/**
+ * Вернуть всех студентов
+ * Read (All) === GET
+ * @param request
+ * @param response
+ * @returns {*}
+ */
 exports.index = function (request, response) {
     return response.status(200).json(students);
 };
--- 22_backStudent/routes/students.js
+++ 22_backStudent/routes/students.js
@@ -4,9 +4,13 @@ let router = express.Router();
 // Получить модуль управления продуктами
 let cStudents = require('./../controllers/studentsController')
 
+// Создать новую запись
+// Create === POST
+router.post('/', cStudents.create)
+
 // назначить маршрут в корень на метод
 // Read (All) === GET
-router.get('/', cStudents.index);
+router.get('/', cStudents.index)
 
 
 // CRUD - каждой операции свой метод и маршрут
