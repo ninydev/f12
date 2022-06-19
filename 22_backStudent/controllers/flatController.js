0e16e21d575113a47fe4616f91efa9c611aca991
Oleksandr Nykytin <keeper@ninydev.com>
2022-06-19 18:25:29 +0300

    Crud for Tags

--- 22_backStudent/controllers/flatController.js
+++ 22_backStudent/controllers/flatController.js
@@ -4,12 +4,27 @@
  * @param response
  */
 
-// Данные, которые я хочу передавать
-let flat = {
+// Формирование кухни
+let kitchen = {
     table: {
-        name : 'Стол в гостинной'
+        name : 'Стол в кухне'
+    },
+    gaz: {
+        burner: [
+            {name: 'Малая'},
+            {name: 'Средняя'},
+            {name: 'Большая'},
+        ]
     }
 }
+
+
+// Данные, которые я хочу передавать
+let flat = [
+    kitchen,
+]
+
+
 
 exports.index = function (request, response) {
     return response.status(200).json(flat)
