022ce55a1ff3ebedb47d1d03e2ca782d01c13889
Oleksandr Nykytin <keeper@ninydev.com>
2022-06-27 18:44:46 +0300

    componentDidMount

--- 24_first_react/src/components/Tags/TagsList.js
+++ 24_first_react/src/components/Tags/TagsList.js
@@ -15,26 +15,51 @@ class TagsList extends React.Component{
         this.state = {
             error: null, // Храним состояние ошибки
             isLoaded: false, // Храним состояние - загрузились ли данные
-            tags : [
+            data: [] // Место, где будут храниться мои данные в данном случае метки
+        }
+
+    }
+
+    // Получение данных с сервера
+    getDataFromServer(){
+        this.setState({
+            data : [
                 {id: 1, name: "Asp"},
                 {id: 2, name: "Php"},
                 {id: 3, name: "C#"},
                 {id: 4, name: "C++"},
                 {id: 5, name: "Html"},
                 {id: 6, name: "Css"}
-            ]
-        }
+            ],
+            isLoaded: true
+        })
+    }
+
+    /*
+    Эта функция будет вызвана лишь раз во всем жизненном цикле данного компонента и будет
+    сигнализировать, что компонент и все его дочерние компоненты отрисовались без ошибок.
+    Т.к. эта функция гарантирована будет вызвана лишь раз, то это превосходный кандидат для
+    выполнения любых сайд-эффектов, как то AJAX запросы.
+     */
+    componentDidMount(){
+        console.log('componentDidMount')
+        // Получение данных для первой загрузки лучше делать тут!!!!!
+        // fetch  - будет тут
+
+        // эмитация задержки получения данных от сервера - с зарежкой на 3 секунды
+        setTimeout (this.getDataFromServer.bind(this), 1000)
 
     }
 
 
+
     // Отвечает за то, как будет выглядеть компонент
     renderData() {
-        console.log('Работает рендер')
+        console.log('Работает рендер - строит элемент с данными')
         return (
             <ul>
                 {
-                    this.state.tags.map( tag => (
+                    this.state.data.map( tag => (
                         <li key={tag.id}>{tag.name}</li>
                     ))
                 }
@@ -44,6 +69,7 @@ class TagsList extends React.Component{
 
     // Я принимаю решение - как я себя отображаю
     render(){
+        console.log('Render - строит внешний вид компонента')
         // Если в компоненте произошла ошибка - вывести ее
         if(this.state.error){
             return this.renderError()
