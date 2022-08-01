c93537c8d3a30c7df70bed6babf13e426aa7c6bb
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-01 21:00:42 +0300

    Email Verify

--- 26_auth/front/src/App.js
+++ 26_auth/front/src/App.js
@@ -2,16 +2,25 @@
 import './App.css';
 import FormLogin from "./components/auth/forms/FormLogin";
 import FormRegister from "./components/auth/forms/FormRegister";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import EmailVerify from "./components/auth/EmailVerify";
 //require('dotenv').config()
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"><BrowserRouter>
+
+        <Routes>
+            <Route path="/registration" element={<FormRegister />} />
+            <Route path="/login" element={<FormLogin />} />
+            <Route path="/auth/verifyEmail/:checkKey" element={<EmailVerify />} />
+        </Routes>
+
         <FormRegister></FormRegister>
         <hr/>
       <FormLogin></FormLogin>
-    </div>
+    </BrowserRouter></div>
   );
 }
 
---
+++ 26_auth/front/src/components/auth/EmailVerify.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+
+class EmailVerify extends React.Component {
+
+    // Цель компонента - получить ключ из строки
+    // отправить его на сервер
+
+    // На сервере поставить дату когда пользователь подтвердил
+    // свою почту
+
+    constructor(props) {
+        super(props);
+        console.log("Props")
+        console.log(props)
+        // const { checkKey } = this.props.match.params.checkKey;
+        // console.log(checkKey)
+        // const params = useParams()
+
+    }
+
+    render() {
+
+        return (
+            <>
+
+            </>
+        )
+    }
+
+}
+
+export default EmailVerify
