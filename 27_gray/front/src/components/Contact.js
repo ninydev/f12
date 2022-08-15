bd66c62732fe9545cbf8fd807b268992ee87e6cd
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-15 19:28:06 +0300

    NovaPoshta full

--- 27_gray/front/src/components/Contact.js
+++ 27_gray/front/src/components/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
                                 <i className="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 className="text-uppercase m-0">Address</h4>
                                 <hr className="my-4 mx-auto"/>
-                                <SelectNovaPoshta></SelectNovaPoshta>
+                                <SelectNovaPoshta someVar="Hello Props"></SelectNovaPoshta>
                                 <hr />
                                 <ClassMethod></ClassMethod><br />
                                 <HookMethod></HookMethod><br />
--- 27_gray/front/src/components/novaPoshta/SelectNovaPoshta.js
+++ 27_gray/front/src/components/novaPoshta/SelectNovaPoshta.js
@@ -1,13 +1,14 @@
 import {useState} from "react";
 
-export default function SelectNovaPoshta() {
+export default function SelectNovaPoshta(props) {
+    // console.log("Props:")
+    // console.log(props)
 
     // Ключ
     const key = "0a97c8c98d19e6e4097fea363302f17b"
 
     // Место хранения областей
-    const [areas, setAreas] = useState([]);
-
+    const [areas, setAreas] = useState([])
     // Метод получения областей
     const loadAreas = () => {
         let data = {
@@ -34,8 +35,86 @@ export default function SelectNovaPoshta() {
     }
 
 
+    const [cities, setCities] = useState([])
+    const changeSelectedArea= (ev) => {
+        // console.log("User Select area: ")
+        // console.log(ev.target.value)
+
+        let data = {
+            "apiKey": key,
+            "modelName": "Address",
+            "calledMethod": "getCities",
+            "methodProperties": {
+                "AreaRef" :ev.target.value,
+            }
+        }
+
+        fetch('https://api.novaposhta.ua/v2.0/json/', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(inCities => {
+                console.log(inCities)
+                setCities(inCities.data)
+            })
+            .catch(err=> {
+                console.log("err")
+                console.log(err)
+            })
+    }
+
+    const [warehouses, setWarehouses] = useState([])
+    const changeSelectedCity = (ev)=> {
+        // console.log("User Select area: ")
+        // console.log(ev.target.value)
+
+        let data = {
+            "apiKey": key,
+            "modelName": "Address",
+            "calledMethod": "getWarehouses",
+            "methodProperties": {
+                "CityRef" :ev.target.value,
+            }
+        }
+
+        fetch('https://api.novaposhta.ua/v2.0/json/', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(inWarehouses => {
+                console.log(inWarehouses)
+                setWarehouses(inWarehouses.data)
+            })
+            .catch(err=> {
+                console.log("err")
+                console.log(err)
+            })
+    }
+
+
 
     return(<>
         <button type="button" onClick={loadAreas}>load</button>
+        <select onChange={changeSelectedArea}>
+            {areas.map(area => (
+                <option key={area.Ref} value={area.Ref}>{area.Description}</option>
+            ))}
+        </select>
+        <select onChange={changeSelectedCity}>
+            {cities.map(city => (
+                <option key={city.Ref} value={city.Ref}>{city.Description}</option>
+            ))}
+        </select>
+        <select>
+            {warehouses.map(warehouse => (
+                <option key={warehouse.Ref} value={warehouse.Ref}>{warehouse.Description}</option>
+            ))}
+        </select>
         </>)
 }
