b2d57426474bbfda08df91cc9e8fbaf9e8100706
Oleksandr Nykytin <keeper@ninydev.com>
2022-08-29 20:12:08 +0300

    Start

--- 27_gray/back/app.js
+++ 27_gray/back/app.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Чтение данных из файла .env
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Сам веб сервер (приложение)
+let express = require('express');
+let app = express();
 
-var app = express();
-
+// Для технических сообщений
+let logger = require('morgan');
 app.use(logger('dev'));
+
+// Для разрешения получать запросы с любого места
+let cors = require('cors')
+app.use(cors())
+
+// Для работы JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Для работы с куками
+let cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+// Для статических файлов
+let path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Для работы с базой данных
+require("./config/mongo").connect();
+
+// Тут будут маршруты
 
 module.exports = app;
