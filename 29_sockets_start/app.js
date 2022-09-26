00bebdb5b5cefa2fd72bc66ec19c02895015db9c
Oleksandr Nykytin <keeper@ninydev.com>
2022-09-26 20:06:22 +0300

    Socket io

--- 29_sockets_start/app.js
+++ 29_sockets_start/app.js
@@ -1,9 +1,9 @@
-//
+// Создадим простой веб сервер для поддержки CORS
 import { createServer } from "http"
 const httpServer = createServer()
 
+// Создадим пассивный сокет - и будем ждать соединений
 import {Server} from "socket.io"
-
 const io = new Server(httpServer, {
     cors: {
         origin: "http://localhost:63342"
@@ -14,8 +14,16 @@ const io = new Server(httpServer, {
  * Поведение нашей системы - когда кто-то пытается присоединиться
  */
 io.on("connection" , (socket) => {
+    // Когда кто-то приходит - отправляем ему сообщение типа Hello
     console.log("New Connection")
-    socket.emit("Hello", "World")
+    // socket.emit("SomeName", "Отправить сообщение этого типа")
+    socket.broadcast.emit("NewConnection" , "Кто то еще пришел на сервер")
+
+    socket.on("newMsg", (args) => {
+        console.log(args)
+        socket.broadcast.emit("ServerNewMessage", args) // Всем кроме себя
+    })
+
 })
 
 httpServer.listen(3000);
