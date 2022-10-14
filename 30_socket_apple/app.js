f8bf020ca2948e2a7dfdb2ad863e765adb0d3445
Oleksandr Nykytin <keeper@ninydev.com>
2022-10-14 20:24:33 +0300

    Apple Socket

--- 30_socket_apple/app.js
+++ 30_socket_apple/app.js
@@ -10,11 +10,35 @@ const io = new Server(httpServer, {
     }
 });
 
+// При открытии магазина у нас 10 яблок
+let appleCount = 10
+
 /**
  * Поведение нашей системы - когда кто-то пытается присоединиться
  */
 io.on("connection" , (socket) => {
     console.log("+ Кто то пришел в магазин")
+
+    // Тому кто пришел говорим сколько у нас яблок
+    socket.send(appleCount)
+
+    // Просто слушаем, что нам говорят
+    socket.on('message', (world) => {
+        console.log('! Кто то сказал: ' + world);
+        // Происходит разбор, что мне сказали
+        if(world === 'plus') {
+            appleCount++
+            socket.broadcast.emit('plus', appleCount)
+        }
+        else {
+            appleCount--
+            socket.broadcast.emit('minus', appleCount)
+        }
+        //*--  Я говорю только одному - кто приходил
+        socket.send(appleCount)
+        //*-- Я говорю всем, кроме того кто приходил
+        socket.broadcast.emit('message', appleCount)
+    })
 
     socket.on('disconnect', function() {
         console.log('- Кто то ушел с магазина');
