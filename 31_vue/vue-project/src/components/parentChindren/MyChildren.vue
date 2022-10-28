3407ddb09b4b4baf6fc4efc4e83e66bfce775053
Oleksandr Nykytin <keeper@ninydev.com>
2022-10-28 20:59:24 +0300

    Vue JS Slots

--- 31_vue/vue-project/src/components/parentChindren/MyChildren.vue
+++ 31_vue/vue-project/src/components/parentChindren/MyChildren.vue
@@ -7,6 +7,13 @@
 const props = defineProps(["count"]);
 console.log("In Children Count: ");
 console.log(props.count);
+
+// const doPlus = () => {
+//   props.count.value++;
+// };
+// const doMinus = () => {
+//   props/count.value--;
+// };
 </script>
 
 <style scoped></style>
